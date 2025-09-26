00354ad4a8bdd0e6a112ac4dad81bf030afc0656
shengshampoo <shengzh@tutanota.com>
2025-09-26 18:11:50 +0000

    Update build-static-zerotier.sh

--- build-static-zerotier.sh
+++ build-static-zerotier.sh
@@ -17,6 +17,7 @@ sed -i -e "/^DESTDIR?=/s/=/=\/usr\/local\/zerotiermm/" ./make-linux.mk
 sed -i -e "s@rustybits/target/release@rustybits/target/${HOST_ARCH}-chimera-linux-musl/release@g" ./make-linux.mk
 sed -i -e "s@rustybits/target/debug@rustybits/target/${HOST_ARCH}-chimera-linux-musl/debug@g" ./make-linux.mk
 RUSTFLAGS="-C target-feature=+crt-static -C linker=clang -C strip=symbols -C opt-level=s"
+ZT_CARGO_FLAGS="-C target-feature=+crt-static -C linker=clang -C strip=symbols -C opt-level=s"
 make ZT_STATIC=1 ZT_NONFREE=1
 make install
 
