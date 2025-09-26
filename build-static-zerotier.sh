fdbb3a23248e263cc234d5f222694bea64475ea8
shengshampoo <shengzh@tutanota.com>
2025-09-26 17:28:00 +0000

    Create build-static-zerotier.sh

---
+++ build-static-zerotier.sh
@@ -0,0 +1,25 @@
+
+#! /bin/bash
+
+set -e
+
+WORKSPACE=/tmp/workspace
+mkdir -p $WORKSPACE
+mkdir -p /work/artifact
+
+HOST_OS=$(uname -s)
+HOST_ARCH=$(uname -m)
+
+git clone https://github.com/zerotier/ZeroTierOne.git
+cd ZeroTierOne
+sed -i -e "s/cargo build/cargo build --target ${HOST_ARCH}-linux-musl/g" ./make-linux.mk
+sed -i -e "/^DESTDIR?=/s/=/=\/usr\/local\/zerotiermm/" ./make-linux.mk
+sed -i -e "s@rustybits/target/release@rustybits/target/${HOST_ARCH}-chimera-linux-musl/release@g" ./make-linux.mk
+sed -i -e "s@rustybits/target/debug@rustybits/target/${HOST_ARCH}-chimera-linux-musl/debug@g" ./make-linux.mk
+make
+make install
+
+cd /usr/local
+tar vcJf ./zerotiermm.tar.xz zerotiermm
+
+mv ./zerotiermm.tar.xz /work/artifact/
